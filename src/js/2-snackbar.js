817a6c5c0e809a63c6f74ca5b0c354966e60e7c1
DinoYaya <kriczkij91@gmail.com>
2025-03-30 16:46:52 +0200

    upd2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+document.querySelector('.form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const delay = parseInt(this.elements.delay.value);
+  const state = this.elements.state.value;
+
+  if (isNaN(delay) || delay < 0) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Delay must be a positive number',
+      position: 'topRight'
+    });
+    return;
+  }
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    });
+    
+  this.reset(); // Очищення форми
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
